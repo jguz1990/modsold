4605e830cddf59f38c1d6b5770b7afc948f6b4a1
Sweets <jose.gman714@gmail.com>
2022-01-03 16:01:49 -0500

    Multiple Changes
    Removed Unused Files
    Changed Ammo Boxes Contents

--- ADVANCED G.E.A.R/media/lua/shared/shared/BackpackAttachments_ItemTweaks.lua
+++
@@ -1,7 +0,0 @@
-if  getActivatedMods():contains("ItemTweakerAPI")  then
-require("ItemTweaker_Core");
-else  return  end
-
-TweakItem("Base.PetrolCan", "AttachmentType", "Gas");
-TweakItem("Base.EmptyPetrolCan", "AttachmentType", "Gas");
-
