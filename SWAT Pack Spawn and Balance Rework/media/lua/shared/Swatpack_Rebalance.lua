a1df7946339fca7981917fe4bc57e14ddf6a9f56
Sweets <jose.gman714@gmail.com>
2022-01-05 07:11:37 -0500

    Multiple Changes
    Added Extra Map Symbols
    Added Sleep With Friends
    Updated Custom Zeds
    Tweaked Items

--- SWAT Pack Spawn and Balance Rework/media/lua/shared/Swatpack_Rebalance.lua
+++ SWAT Pack Spawn and Balance Rework/media/lua/shared/Swatpack_Rebalance.lua
@@ -22,10 +22,6 @@ if item then
 	item:DoParam("WindResistance = 0.30")
 	item:DoParam("WaterResistance = 0.35")
 end
-local item = ScriptManager.instance:getItem("Base.Shoes_SwatBoots")	
-if item then 
-	item:DoParam("RunSpeedModifier = 0.9")
-end
 local item = ScriptManager.instance:getItem("Base.Gloves_SwatGloves")	
 if item then 
 	item:DoParam("ScratchDefense = 30")
@@ -33,23 +29,13 @@ if item then
 	item:DoParam("Insulation = 0.25")
 	item:DoParam("WindResistance = 0.3")
 end
-local item = ScriptManager.instance:getItem("Base.Gloves_RiotGloves")	
-if item then 
-	item:DoParam("ScratchDefense = 65")
-	item:DoParam("BiteDefense = 50")
-end
 local item = ScriptManager.instance:getItem("Base.Hat_SwatHelmet")	
 if item then 
 	item:DoParam("Insulation = 0.25")
 	item:DoParam("WaterResistance = 0.2")
-	item:DoParam("ChanceToFall = 10")
 end
 local item = ScriptManager.instance:getItem("Base.Vest_BulletSwat")	
 if item then 
-	item:DoParam("BiteDefense = 40")
-	item:DoParam("ScratchDefense = 65")
-	item:DoParam("Insulation = 0.65")
-	item:DoParam("WindResistance = 0.30")
 	item:DoParam("WorldStaticModel = BulletVest_Ground")
 end
 local item = ScriptManager.instance:getItem("Base.Jacket_Swat")	
@@ -62,7 +48,6 @@ if item then
 end
 local item = ScriptManager.instance:getItem("Base.Glasses_SwatGoggles")	
 if item then 
-	item:DoParam("CombatSpeedModifier = 1")
 	item:DoParam("BiteDefense = 10")
 	item:DoParam("ScratchDefense = 10")
 end	
@@ -70,25 +55,9 @@ local item = ScriptManager.instance:getItem("Base.Hat_Balaclava_Swat")
 if item then 
 	item:DoParam("WaterResistance = 0.5")
 end	
-local item = ScriptManager.instance:getItem("Base.Hat_Antibombhelmet")	
-if item then 
-	item:DoParam("ChanceToFall = 5")
-end	
 
-local item = ScriptManager.instance:getItem("Base.SWATPouch")	
-if item then 
-	item:DoParam("Capacity = 20")
-end
 
-local item = ScriptManager.instance:getItem("Base.SwatElbowPads")	
-if item then 
-	item:DoParam("CombatSpeedModifier = 0.95")
-end
 
-local item = ScriptManager.instance:getItem("Base.SwatKneePads")	
-if item then 
-	item:DoParam("RunSpeedModifier = 0.95")
-end
 
 
 
