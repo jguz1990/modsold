8ed508cd1f28440edd615e43836f10537638c990
Sweets <jose.gman714@gmail.com>
2022-01-08 13:05:31 -0500

    Added Primitive Survival
    https://steamcommunity.com/sharedfiles/filedetails/?id=1969674962

---
+++ PrimitiveSurvival/media/lua/server/Recipes.lua
@@ -0,0 +1,10 @@
+function recipe_smallrope(items, result, player)
+	local inv = player:getInventory();
+	inv:AddItem("PrimitiveSurvival.PS_SmallRope");
+end 
+
+function recipe_smallrope2(items, result, player)
+	local inv = player:getInventory();
+	inv:AddItem("PrimitiveSurvival.PS_SmallRope");
+	inv:AddItem("PrimitiveSurvival.PS_SmallRope");
+end 
