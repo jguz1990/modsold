3f5b0b036daf6245e7e56449ea6fc4e87508d8b2
Sweets <jose.gman714@gmail.com>
2022-01-17 13:47:05 -0500

    Removed Westpoint Expansion

--- west_point_expansion/media/maps/west_point_expansion/spawnregions.lua
+++
@@ -1,8 +0,0 @@
-function SpawnRegions()
-    return {
-        { name = "Muldraugh, KY", file = "media/maps/Muldraugh, KY/spawnpoints.lua" },
-        { name = "West Point, KY", file = "media/maps/West Point, KY/spawnpoints.lua" },
-        { name = "Bedford Falls", file = "media/maps/BedfordFalls/spawnpoints.lua" },
-        { name = "West Point Expansion", file = "media/maps/west_point_expansion/spawnpoints.lua" },
-    }
-end
