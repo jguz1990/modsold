bf19f82c0e38b34f1143da7efc73dbfa46343a66
Sweets <jose.gman714@gmail.com>
2022-01-18 20:07:35 -0500

    deconflict

--- Hiker/media/lua/client/Backpacker.lua
+++ Hiker/media/lua/client/Backpacker.lua
@@ -1,13 +1,13 @@
 require('NPCs/MainCreationMethods');
-local function initHikerTrait()	
-	local hiker = TraitFactory.addTrait("hiker", getText("UI_trait_Hiker"), 2, getText("UI_trait_HikerDesc"), false, false);
+local function initStrongbackTrait()	
+	local strongback = TraitFactory.addTrait("strongback", getText("UI_trait_Strongback"), 2, getText("UI_trait_StrongbackDesc"), false, false);
 end
 
-Events.OnGameBoot.Add(initHikerTrait);
+Events.OnGameBoot.Add(initStrongbackTrait);
 
 Events.OnGameStart.Add(function()
-	if getPlayer():HasTrait("hiker") then
+	if getPlayer():HasTrait("strongback") then
 		local maxWeightBase = getPlayer():getMaxWeightBase();
-        getPlayer():setMaxWeightBase(maxWeightBase * 1.5);
+        getPlayer():setMaxWeightBase(maxWeightBase * 2);
     end
 end)
