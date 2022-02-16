1a27d2af8e0307b9233ffd3ae48cb5a62b144065
Sweets <jose.gman714@gmail.com>
2022-02-16 16:47:10 -0500

    Military Airport

---
+++ Military Airport/media/maps/militaryairport/spawnpoints.lua
@@ -0,0 +1,22 @@
+function SpawnPoints()
+return {
+  constructionworker = {
+    { worldX = 33, worldY = 27, posX = -21, posY = 161, posZ = 0 }
+  },
+  fireofficer = {
+    { worldX = 33, worldY = 27, posX = -21, posY = 161, posZ = 0 }
+  },
+  parkranger = {
+    { worldX = 33, worldY = 27, posX = -21, posY = 161, posZ = 0 }
+  },
+  policeofficer = {
+    { worldX = 33, worldY = 27, posX = -21, posY = 161, posZ = 0 }
+  },
+  securityguard = {
+    { worldX = 33, worldY = 27, posX = -21, posY = 161, posZ = 0 }
+  },
+  unemployed = {
+    { worldX = 33, worldY = 27, posX = -21, posY = 161, posZ = 0 }
+  }
+}
+end
